9435ac44a29fe9c7cee31ab01ebf039612c730ee
Guillaume Meheut <gmeheut@fr-b-tb05174.home>
2022-02-23 13:22:41 +0100

    document

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="description" content="" />
+          <meta name="keywords" content="" />
+          <meta name="author" content="The Cryptoids" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
